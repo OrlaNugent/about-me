a31235337b5ff6ad5c25b3a6a3f8b621b2637f06
OrlaNugent <orla_nugent@hotmail.co.uk>
2024-02-18 21:32:22 +0000

    packages, app styling and structure

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import "./fonts/Montserrat/static/Montserrat-Medium.ttf";
 import "./fonts/Montserrat/static/Montserrat-Bold.ttf";
+import "./fonts/Playfair_Display/static/PlayfairDisplay-Bold.ttf";
+import "./fonts/Playfair_Display/static/PlayfairDisplay-Medium.ttf";
 import { NavBar } from "./components/NavBar/NavBar";
+import { Skills } from "./components/Skills/Skills";
 import { Hero } from "./components/Hero/Hero";
+import { Contact } from "./components/Contact/Contact";
 
 function App() {
 	return (
 		<div className='App'>
 			<NavBar />
 			<Hero />
+			<Skills />
+			<Contact />
 		</div>
 	);
 }
